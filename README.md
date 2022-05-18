6a8632926eaa0cb9ca1899ecd59e616d83ad1bba
mfa69 <mfa69@sfu.ca>
2022-05-17 17:22:31 -0700

    Added README and cleaned up directory

---
+++ README.md
@@ -0,0 +1,8 @@
+Make sure you have a csv file called `raw_scores.csv`
+In `raw_scores.csv` you should have a column with all the team names, and the scores for each week in a column under the dates. If a team has no score for the week (they did not show up), do not enter any values.
+
+To RUN:
+`python3 adjust_scores.py`
+
+This generates a csv file called `adjsuted_scores.csv` which contains the adjsuted scores for each week and the running total.
+
--- adjust_scores.py
+++ adjust_scores.py
@@ -1,36 +1,72 @@
 import pandas as pd
-import dataframe_image as dfi
-import sys
+import numpy as np
+
+# Assign the base score to each team that week
+# 35 for winner, 34 second, ...
+# In the case of a draw, the two teams get the same base score
+# Ex. If second and third place draw,
+# 1st will get 35 pts, 2nd 34 pts, 3rd 34 pts, 4th 33 pts, etc
 
 MAX_SCORE = 35
 
+def get_unique_weekly_scores(weekly_scores):
+    unique_scores = pd.Series(sorted(set(weekly_scores), reverse = True)).dropna()
+    return unique_scores.values
 
-def convert_scores(scores):
-    unique_scores = sorted(set(scores['score']), reverse = True)
-    scores['base_score'] = scores['score'].apply(lambda x: MAX_SCORE - unique_scores.index(x))
-    scores['bonus_score'] = scores['score'].apply(lambda x: scores.shape[0] - scores[scores['score'] >= x].shape[0])
-    scores['adjusted_score'] = scores['base_score'] + scores['bonus_score']
+def get_base_scores(weekly_scores):
 
-    return scores
+    # get the unique scores from the week
+    unique_scores = get_unique_weekly_scores(weekly_scores)
 
+    # get the correct number of base scores for the week
+    n = get_unique_weekly_scores(weekly_scores).shape[0]
+    base_scores = np.arange(MAX_SCORE, MAX_SCORE-n, -1)
 
-def create_dataframe_image(scores, outfile):
-    print_scores = pd.DataFrame({'Team': scores['team'], 'Score': scores['adjusted_score']}).set_index('Team')
-    dfi.export(print_scores, outfile, table_conversion = 'matplotlib')
+    # Create dictionary of scores to base score
+    scores_dict = dict(zip(unique_scores, base_scores))
+    scores_dict.update({0.0: 0})
 
+    weekly_scores = weekly_scores.fillna(0)
 
-def main(date, infile, outfile):
-    scores = pd.read_csv('scores/2022_04_13.csv')
-    print(scores)
-    latest_scores = pd.read_csv(infile).sort_values(by=['score'], ascending = False)
-    adjusted_scores = convert_scores(latest_scores)
-    adjusted_scores.to_csv(outfile)
+    return weekly_scores.apply(lambda x: scores_dict[x])
 
 
-    create_dataframe_image(adjusted_scores, 'tables/' + date + '.png')
+
+# Assign the bonus score to each team that week
+# Bonus = Number of teams you beat
+# In the case of a draw, you did not beat that team
+
+def get_bonus_scores(weekly_scores):
+    num_teams = weekly_scores[weekly_scores > 0].count()
+    weekly_scores = weekly_scores.apply(lambda x: weekly_scores[weekly_scores < x].count())
+    return weekly_scores
+
+
+# Take a column of weekly scores and convert them
+def adjust_weekly_scores(weekly_scores):
+    adjusted_scores = get_base_scores(weekly_scores) + get_bonus_scores(weekly_scores)
+    return adjusted_scores
+
+
+
+# Adjust the scores of all the columns
+def adjust_all_scores(scores_df):
+    for weekly_score in scores_df:
+        weekly_scores = scores_df[weekly_score]
+        scores_df[weekly_score] = adjust_weekly_scores(weekly_scores)
+    return scores_df
+
+
+def main():
+    scores = pd.read_csv('raw_scores.csv')
+    scores = scores.dropna(axis=1, how='all')
+    scores = scores.set_index('team')
+
+    scores = adjust_all_scores(scores)
+    scores['Total'] = scores.sum(axis = 1)
+    scores.sort_values(by='Total', ascending = False, inplace = True)
+
+    scores.to_csv('adjusted_scores.csv')
 
 if __name__ == '__main__':
-    date = str(sys.argv[1])
-    infile = 'scores/' + date + '.csv'
-    outfile = 'adjusted-scores/' + date + '_adjusted.csv'
-    main(date, infile, outfile)
+    main()
